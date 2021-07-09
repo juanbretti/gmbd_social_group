a5b86d737a2d97075d036ab4e395a5989a0a9964
Juan Pedro Bretti Mandarano <juan+github@bretti.com.ar>
2021-07-10 00:10:47 +0200

    update gitignore

--- data/.history/encoding_20210709083629.py
+++
@@ -1,6 +0,0 @@
-# look at the first ten thousand bytes to guess the character encoding
-with open("data_by_artist.csv", 'rb') as rawdata:
-    result = chardet.detect(rawdata.read(10000))
-
-# check what the character encoding might be
-print(result)
